cde7995c0683a533aebfd98615f97d88464bd2a0
Expero ALOFA <expealofa@gmail.com>
2026-07-29 16:53:15 +0100

    comparaison avec Excelsss

--- src/lib/simulation/simulation.ts
+++ src/lib/simulation/simulation.ts
@@ -175,7 +175,7 @@ function calculerPrimeParcelle(
   const PAC = PAI / (1 - fa);
   const a_m = (1 - v) / (1 - Math.pow(v, 1 / freq));
 
-  return P_epargne;
+  return P_call;
   // return PAC / a_m;
 }
 
